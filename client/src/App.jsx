8ceff142c91d9719ce7dda5fe5a78b87fcdcb750
Nisha <nisha.rohilla@gmail.com>
2023-01-19 12:38:49 +0100

    showcard feature added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 
 import './App.css'
+import ShowAPI from "./ShowAPI.jsx";
+import ShowCard from "./ShowCard.jsx";
+import {useState} from "react";
+
 
 function App() {
 
+  const [showData, setShowData]= useState(ShowAPI);
 
   return (
-    <>
-    </>
+      <>
+
+        <ShowCard showData={showData}/>
+
+      </>
   )
 }
 
 export default App
+
+
