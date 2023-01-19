470693e69b7a4d2f316f4ef97dd5b1b1fdb8e70e
StromTina <112858034+StromTina@users.noreply.github.com>
2023-01-19 09:45:20 +0100

    finished responsive navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     return (
         <>
             <div className='navigationBar'>
-                <img className="logo" src={logo}></img>
-                <h1> Show some REACTion </h1>
-                <div className="navLinks">
-                    <a href='/' className='headerLink' id='logIn'> Log in</a>
-                    <a href='/' className='headerLink' id='signUp'>Sign up</a>
+                <img className='logo' src={logo}></img>
+                <h1 className='pageTitle'> Show some REACTion </h1>
+                <div className='navLinks'>
+                    <a href='/' className='navLink' id='logIn'> Log in</a>
+                    <a href='/' className='navLink' id='signUp'>Sign up</a>
                 </div>
             </div>
 
