4a965a4948cb4f30d1f188de4888bf7cea2e2437
Mukhtar Sibai <mukhtar@Mukhtars-Laptop.local>
2023-01-21 18:21:23 +0100

    Creating SearchByDate feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import SearchByName from "./SearchByName.jsx";
 import SearchByPrice from "./SearchByPrice.jsx";
 import { Routes, Route } from "react-router-dom";
 import EventPage from "./EventPage.jsx";
+import SortByDate from "./SearchByDate.jsx";
 
 function App() {
 
@@ -19,25 +20,26 @@ function App() {
         <>
 
             <Navbar/>
-            {/*<SearchByName showData={showData} setFilteredData={setFilteredData}/>*/}
-            {/*<SearchByPrice showData={showData} setFilteredData={setFilteredData}/>*/}
-            {/*{filteredData ?*/}
-            {/*    (*/}
-            {/*        <ShowCard showData={filteredData} />*/}
-            {/*    ) :*/}
-            {/*    (*/}
-            {/*        <ShowCard showData={showData} />*/}
+            <SearchByName showData={showData} setFilteredData={setFilteredData}/>
+            <SortByDate showData={showData} setShowData={setShowData} stFilteredData={setFilteredData} />
+            <SearchByPrice showData={showData} setFilteredData={setFilteredData}/>
+            {filteredData ?
+                (
+                    <ShowCard showData={filteredData} />
+                ) :
+                (
+                    <ShowCard showData={showData} />
 
-            {/*    )*/}
-            {/*}*/}
-            <Routes>
-                <Route path="/" element={<ShowCard showData={showData} />} />
-                <Route path="EventPage" element={<EventPage />} />
-            </Routes>
-            <Routes>
-                <Route path="/" element={<ShowCard showData={showData} />} />
-                <Route path="EventPage" element={<EventPage />} />
-            </Routes>
+                )
+            }
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<ShowCard showData={showData} />} />*/}
+            {/*    <Route path="EventPage" element={<EventPage />} />*/}
+            {/*</Routes>*/}
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<ShowCard showData={showData} />} />*/}
+            {/*    <Route path="EventPage" element={<EventPage />} />*/}
+            {/*</Routes>*/}
 
             <Footer/>
 
