80cdfbb065639a2efd4663771ae0649e10617ff5
Mukhtar Sibai <mukhtar.sibai@outlook.com>
2023-01-24 16:15:59 +0100

    Refactoring router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,28 @@
-import './App.css'
-import Footer from "./Footer.jsx";
-import React from "react";
-import Navbar from './Navbar';
+import "./App.css";
+import React, { useState } from "react";
 import ShowAPI from "./ShowAPI.jsx";
-import ShowCard from "./ShowCard.jsx";
-import {useState} from "react";
-import SearchByName from "./SearchByName.jsx";
-import SearchByPrice from "./SearchByPrice.jsx";
-import { Routes, Route } from "react-router-dom";
-import EventPage from "./EventPage.jsx";
-import AddressBox from "./AddressBox.jsx";
-import SearchByDate from "./SearchByDate.jsx";
 import Filter from "./Filter.jsx";
+import ShowCard from "./ShowCard.jsx";
 
 function App() {
+  const [showData, setShowData] = useState(ShowAPI);
+  const [filteredData, setFilteredData] = useState(null);
 
-    const [showData, setShowData] = useState(ShowAPI);
-    const [filteredData, setFilteredData] = useState(null);
-
-    return (
-        <>
-            <Navbar/>
-
-
-
-            <Routes>
-                <Route path="/" element={<> <Filter showData={showData} setShowData={setShowData}
-                                                    setFilteredData={setFilteredData}/>
-                    {filteredData ?
-                    (<ShowCard showData={filteredData} />)
-                    :
-                    (<ShowCard showData={showData} />)
-                }</>}/>
-
-                <Route path="EventPage" element={<EventPage />} />
-                <Route path="AddressBox" element={<AddressBox />} />
-            </Routes>
-
-            <Footer/>
-
-        </>
-
-    )
+  return (
+    <>
+      <Filter
+        showData={showData}
+        setShowData={setShowData}
+        setFilteredData={setFilteredData}
+      />
+      gi
+      {filteredData ? (
+        <ShowCard showData={filteredData} />
+      ) : (
+        <ShowCard showData={showData} />
+      )}
+    </>
+  );
 }
 
-export default App
-
-
+export default App;
