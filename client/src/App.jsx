c19faac65f3ba7f0da563c52b9a8649cb2660cea
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-01-26 19:12:59 +0100

    Changed logo link and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Filter from "./Filter.jsx";
 import ShowCard from "./ShowCard.jsx";
 import DropDownSearch from "./DropDownSearch.jsx";
 
+
 function App() {
   const [showData, setShowData] = useState(ShowAPI);
   const [filteredData, setFilteredData] = useState(null);
