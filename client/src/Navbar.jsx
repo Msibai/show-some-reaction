81b10868c17cdafc881ed76d4fae46ce582eaa0b
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-01-27 10:13:23 +0100

    Update Navbar.jsx

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -6,35 +6,9 @@ import { BrowserRouter, Link, NavLink, useNavigate } from "react-router-dom";
 function SignUp() {
   window.open("Register", "_self");
 }
-
-
-function goToHome(){
-    window.open('/','_self')
-
-
 function goToHome(){
     window.open('/','_self')
 }
-
-const Navbar = () => {
-    return (
-        <>
-            <div className='navigationBar'>
-                <img className='logo' src={logo} onClick={goToHome}></img>
-
-                <h1 className='pageTitle'> Show some REACTion </h1>
-                <div className='navLinks'>
-                    <a href='/' className='navLink' id='logIn'> Log in</a>
-                    <a href='/' className='navLink' id='signUp'>Sign up</a>
-
-                </div>
-            </div>
-
-        </>
-    )
-
-}
-
 const Navbar = () => {
   return (
     <BrowserRouter>
