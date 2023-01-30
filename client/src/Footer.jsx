39e8f3271464ae64739e63f4c67dc6468cb55db1
sebastianp85 <109860660+sebastianp85@users.noreply.github.com>
2023-01-30 09:14:55 +0100

    Sign up page

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,55 +1,56 @@
 import React from "react";
-import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { BrowserRouter, Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <>
+  return (
+    <>
+      <footer className="footer">
+        <nav>
+          <a href="#" className="footer-logo">
+            <img className="logo-footer" alt="logo" src="src/images/logo.jpg" />
+          </a>
+          <p className="copyright">
+            Copyright &copy; <span className="year">2023</span> by Show some
+            REACTion
+          </p>
+        </nav>
 
-            <footer className="footer">
-                <nav>
-                    <a href="#" className="footer-logo">
-                        <img className="logo-footer" alt="logo" src="src/images/logo.jpg"/>
-                    </a>
-                    <p className="copyright">
-                        Copyright &copy; <span className="year">2023</span> by Show some REACTion
-                    </p>
-                </nav>
+        <nav>
+          <p className="footer-heading">Follow us</p>
+          <ul className="footer-icons">
+            <li>
+              <a className="footer-icon" href="#">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a className="footer-icon" href="#">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a className="footer-icon" href="#">
+                <FaTwitter />
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-                <nav>
-                    <p className="footer-heading">Follow us</p>
-                    <ul className="footer-icons">
-                        <li>
-                            <a className="footer-link" href="#"
-                            >
-                                <FaFacebook/>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="footer-link" href="#"
-                            >
-                                <FaInstagram/>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="footer-link" href="#"
-                            >
-                                <FaTwitter/>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+        <nav>
+          <p className="footer-heading">Account</p>
+          <ul className="footer-nav">
+            <li>
+              <button className="footer-link">Sign Up</button>
+            </li>
+            <li>
+              <button className="footer-link">Sign In</button>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+};
 
-                <nav>
-                    <p className="footer-heading">Account</p>
-                    <ul className="footer-nav">
-                        <li><a className="footer-link" href="#">Create account</a></li>
-                        <li><a className="footer-link" href="#">Sign in</a></li>
-                    </ul>
-                </nav>
-            </footer>
-
-        </>
-    )
-}
-
-export default Footer
+export default Footer;
